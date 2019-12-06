d5e5f33ba37d8add538da6aa873f7fbcd3ff1d91
Anton Vyshnevskyi <anton_vyshnevskyi@epam.com>
2019-12-06 11:48:19 +0200

    securing config server

--- config-server/src/main/java/com/suamo/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/com/suamo/configserver/ConfigServerApplication.java
@@ -3,6 +3,10 @@ package com.suamo.configserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
 @EnableConfigServer
@@ -12,4 +16,17 @@ public class ConfigServerApplication {
         SpringApplication.run(ConfigServerApplication.class, args);
     }
 
+
+    @EnableWebSecurity
+    @Configuration
+    public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http
+                    .authorizeRequests().anyRequest()
+                    .authenticated()
+                    .and()
+                    .httpBasic();
+        }
+    }
 }
