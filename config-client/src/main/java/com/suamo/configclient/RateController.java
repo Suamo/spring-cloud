b88ebafb7bb67cfa1add4dbb6ddcbf28e4afb8db
Anton Vyshnevskyi <anton_vyshnevskyi@epam.com>
2019-12-09 12:49:06 +0200

    adding config refresh support

--- config-client/src/main/java/com/suamo/configclient/RateController.java
+++ config-client/src/main/java/com/suamo/configclient/RateController.java
@@ -1,11 +1,13 @@
 package com.suamo.configclient;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@RefreshScope
 public class RateController {
 
     @Value("${rate}")
