904aa039c7bf71f15e642aabf1247fabcb92fe9a
Anton Vyshnevskyi <anton_vyshnevskyi@epam.com>
2019-12-13 17:54:42 +0200

    more of everything

--- cloud-task-sink/src/main/java/com/suamo/tasksink/TaskSinkApplication.java
+++ cloud-task-sink/src/main/java/com/suamo/tasksink/TaskSinkApplication.java
@@ -4,11 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.task.launcher.annotation.EnableTaskLauncher;
 
-@SpringBootApplication
-
 // here spring boot will know how to hook this up to Cloud Bus (message bus in RabbitMQ)
 // Deployer will be used to pull the right task
 @EnableTaskLauncher
+@SpringBootApplication
 public class TaskSinkApplication {
 
 	public static void main(String[] args) {
