022a2d71eedfd7aa231f7a5ae12125f8614d56d2
Anton Vyshnevskyi <anton_vyshnevskyi@epam.com>
2020-01-10 17:35:35 +0200

    streaming

--- coordination-modules/fastpass-console/src/main/java/com/suamo/fastpassconsole/TollPublisher.java
+++ coordination-modules/fastpass-console/src/main/java/com/suamo/fastpassconsole/TollPublisher.java
@@ -2,14 +2,40 @@ package com.suamo.fastpassconsole;
 
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.messaging.Source;
+import org.springframework.context.annotation.Bean;
 import org.springframework.integration.annotation.InboundChannelAdapter;
+import org.springframework.integration.annotation.Poller;
+import org.springframework.integration.core.MessageSource;
+import org.springframework.integration.support.MessageBuilder;
 
-@EnableBinding(Source.class)
+import java.util.Random;
+
+//@EnableBinding(Source.class)
+@EnableBinding(TollSource.class)
 public class TollPublisher {
 
-    @InboundChannelAdapter(channel = Source.OUTPUT)
-    public String sendTollCharge() {
-        return "{station:\"20\", customerid:\"100\", timestamp:\"2017-07-12T03:15:00\"}";
+    Random r = new Random();
+
+
+    @Bean
+//    @InboundChannelAdapter(channel = Source.OUTPUT)
+    @InboundChannelAdapter(channel = "fastpassTollChannel", poller = @Poller(fixedDelay = "2000"))
+    public MessageSource<Toll> sendTollCharge() {
+//        return "{station:\"20\", customerid:\"100\", timestamp:\"2017-07-12T03:15:00\"}";
+
+        Toll payload = new Toll("20", "100", "2017-07-12T03:15:00");
+        return () -> MessageBuilder.withPayload(payload).build();
     }
 
+    static class Toll {
+        public String stationId;
+        public String customerId;
+        public String timestamp;
+
+        public Toll(String stationId, String customerId, String timestamp) {
+            this.stationId = stationId;
+            this.customerId = customerId;
+            this.timestamp = timestamp;
+        }
+    }
 }
--- stream-modules/stream-tollintake/src/main/java/com/suamo/streamtollintake/StreamTollintakeApplication.java
+++ stream-modules/stream-tollintake/src/main/java/com/suamo/streamtollintake/StreamTollintakeApplication.java
@@ -4,10 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Processor;
 import org.springframework.cloud.stream.messaging.Sink;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.messaging.handler.annotation.SendTo;
 
 @SpringBootApplication
 @EnableBinding(Sink.class)
+//@EnableBinding(Processor.class)
 public class StreamTollintakeApplication {
 
     public static void main(String[] args) {
@@ -15,7 +19,11 @@ public class StreamTollintakeApplication {
     }
 
     @StreamListener(Sink.INPUT)
+//    @ServiceActivator(inputChannel = Sink.INPUT)
+//    @StreamListener(Processor.INPUT)
+//    @SendTo(Processor.OUTPUT)
     public void log(String msg) {
         System.out.println(msg);
+//        return msg;
     }
 }
